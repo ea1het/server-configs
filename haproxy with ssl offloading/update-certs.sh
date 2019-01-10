74a802bcdbc98ca70934dda4ec1bca0a92a025b8
Jonathan <j@0x30.io>
2019-01-10 01:05:09 +0100

    Update update-certs.sh

--- haproxy with ssl offloading/update-certs.sh
+++ haproxy with ssl offloading/update-certs.sh
@@ -10,13 +10,13 @@
 ###
 
 # STEP 1: Renew the certificate
-certbot renew --force-renewal --tls-sni-01-port=8888
+certbot renew --force-renewal --preferred-challenges http
 
 # STEP 2: List certbot certificates
 certbot certificates > /tmp/tmp.tmp
 
 # STEP 3: Create a PEM file
-for i in `grep "Domains" /tmp/tmp.tmp  | awk -F ":" '{ print $2 }'`
+for i in `grep "Name" /tmp/tmp.tmp  | awk -F ":" '{ print $2 }'`
 do
   cat /etc/letsencrypt/live/$i/fullchain.pem /etc/letsencrypt/live/$i/privkey.pem > /etc/letsencrypt/live/$i/crtkey.pem
   echo $i
@@ -25,3 +25,5 @@ done
 # STEP 4: Clean-up
 rm -f /tmp/tmp.tmp
 
+# STEP 5: Restart service
+/etc/init.d/haproxy restart
